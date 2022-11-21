e29fc5128327cd87a446d30f14bfca485720fb01
TanyaKuchak <tanyakuchak@ukr.net>
2022-11-21 08:49:41 +0200

    4

---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,41 @@
+import PropTypes from 'prop-types';
+import {
+  Table,
+  TableHeader,
+  TableData,
+  TableRow,
+} from './TransactionHistory.styled';
+export const TransactionHistory = ({ items }) => {
+  return (
+    <Table className="transaction-history">
+      <thead>
+        <tr>
+          <TableHeader>Type</TableHeader>
+          <TableHeader>Amount</TableHeader>
+          <TableHeader>Currency</TableHeader>
+        </tr>
+      </thead>
+
+      <tbody>
+        {items.map(({ id, type, amount, currency }) => (
+          <TableRow key={id}>
+            <TableData>{type.toUpperCase()}</TableData>
+            <TableData>{amount}</TableData>
+            <TableData>{currency}</TableData>
+          </TableRow>
+        ))}
+      </tbody>
+    </Table>
+  );
+};
+
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
---
+++ src/components/TransactionHistory/TransactionHistory.styled.js
@@ -0,0 +1,25 @@
+import styled from '@emotion/styled';
+
+export const Table = styled.table`
+  background-color: ${p => p.theme.colors.white};
+  width: 600px;
+  box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.12), 0px 1px 1px rgba(0, 0, 0, 0.14),
+    0px 2px 1px rgba(0, 0, 0, 0.2);
+`;
+
+export const TableHeader = styled.th`
+  padding: ${p => p.theme.space[4]}px;
+  border: ${p => p.theme.borders.normal};
+  background-color: #63abf2;
+`;
+
+export const TableData = styled.td`
+  padding: ${p => p.theme.space[4]}px;
+  border: ${p => p.theme.borders.normal};
+`;
+
+export const TableRow = styled.tr`
+  :nth-of-type(2n + 1) {
+    background-color: ${p => p.theme.colors.backgroundBody};
+  }
+`;
